c6fbd01fd35d87fae8ab3770e750b1945dffdf53
CoolSrj06 <srijanmaurya6602@gmail.com>
2023-07-27 17:00:17 +0530

    Pusing my second Code

---
+++ Taking_Input.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Taking_Input {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number");
+        int s=sc.nextInt();
+        System.out.println(s);
+    }
+}
