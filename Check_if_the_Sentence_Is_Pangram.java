9b4bf04fe065eaeb493bda9ae45a212b319f87d8
CoolSrj06 <srijanmaurya6602@gmail.com>
2023-07-31 02:15:42 +0530

    Coded on 31 July

--- Check_if_the_Sentence_Is_Pangram.java
+++ Check_if_the_Sentence_Is_Pangram.java
@@ -1,14 +1,29 @@
 public class Check_if_the_Sentence_Is_Pangram {
     public static void main(String[] args) {
+        System.out.println(checkIfPangram("thequickbrownfoxjumpsoverthelazydog"));
+    }
+//    public static boolean checkIfPangram(String sentence) {
+//        return AtoZ(sentence, 'a', 0) == 26;
+//    }
+//    public static int AtoZ(String sentence,char Alphabet,int count){
+//        if(Alphabet>'z')
+//            return count;
+//        count+=StringLoop(sentence.toLowerCase(),0,0,Alphabet);
+//        return AtoZ(sentence,++Alphabet,count);
+//    }
+//    public static int StringLoop(String sentence,int i,int count,char ch){
+//        if (count !=0)
+//            return count;
+//        if (i==sentence.length())
+//            return count;
+//        if(sentence.charAt(i)==ch) {
+//            count += 1;
+//        }
+//        return StringLoop(sentence,i+1,count,ch);
+//    }
+
+   public static boolean checkIfPangram(String sentence) {
 
     }
-    public static boolean checkIfPangram(String sentence) {
-
-    }
-    public static int recursion(String sentence,int i,int ans){
-        if(i==sentence.length())
-            return ans;
-        checker(sentence.charAt(i),)
-    }
-    public static boolean checker(char ch,)
+    public static
 }
--- Create_Target_Array_in_the_Given_Order.java
+++ Create_Target_Array_in_the_Given_Order.java
@@ -26,3 +26,39 @@ public class Create_Target_Array_in_the_Given_Order {
         return target;
     }
 }
+
+/**
+ We can optimize the code to achieve a linear time complexity of O(n) by avoiding unnecessary element shifting in the target array.
+ Instead of using recursion and shifting elements, we can use an ArrayList to build the target array in a single pass.
+ Here's the updated code:
+ */
+
+
+/*
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Create_Target_Array_in_the_Given_Order {
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 7, 5, 7};
+        int[] index = {0, 1, 1, 0, 4};
+        System.out.println(Arrays.toString(createTargetArray(nums, index)));
+    }
+
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        ArrayList<Integer> targetList = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            targetList.add(index[i], nums[i]);
+        }
+
+        int[] target = new int[targetList.size()];
+        for (int i = 0; i < targetList.size(); i++) {
+            target[i] = targetList.get(i);
+        }
+
+        return target;
+    }
+}
+
+ */
